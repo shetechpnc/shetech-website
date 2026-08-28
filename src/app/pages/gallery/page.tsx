95d457255315643ef1c2366b9c09cab5f3597402
shresthamamita28 <shresthamamita28@gmail.com>
2026-08-28 14:05:09 +0545

    Update team and gallery photos

--- src/app/pages/gallery/page.tsx
+++ src/app/pages/gallery/page.tsx
@@ -121,6 +121,7 @@ const Gallery = () => {
               <option value="Best Match">Best Match</option>
               <option value="2024/25">Tenure 2024/25</option>
               <option value="2025/26">Tenure 2025/26</option>
+              <option value="2026/27">Tenure 2026/27</option>
             </select>
 
             {/* Chevron Icon */}
